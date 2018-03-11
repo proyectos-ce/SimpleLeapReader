df7516b7b2f6787d7736577f693c0b09a0f42660
Gustavo Segura <yo@gustavosegura.com>
2018-03-11 16:51:06 -0600

    [add] Simple MQTT publisher 	* Create a dictionary with the basic frame info (id, detected hands, etc) 	* Publish the dictionary in json format to the MQTT brocker

--- simpleLeap.py
+++ simpleLeap.py
@@ -1,12 +1,16 @@
 import Leap, sys, time, thread, math
+import paho.mqtt.client as mqtt
+
 
 class LeapMotionListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+    client = mqtt.Client("leapTavo")
 
     def on_init(self, controller):
         print("Initialized")
+        self.client.connect("iot.eclipse.org", 1883, 60)
 
     def on_connect(self, controller):
         print("Motion Sensor Connected")
@@ -19,10 +23,17 @@ class LeapMotionListener(Leap.Listener):
 
     def on_frame(self, controller):
         frame = controller.frame()
-        print (frame.id,"Is valid:",frame.is_valid)
+        # print (frame.id,"Is valid:",frame.is_valid)
+        frameJson = {'frameId':frame.id, 'hands':[]}
         for hand in frame.hands:
-            handtype = "Left Hand" if hand.is_left else "Right Hand"
+            handtype = "left" if hand.is_left else "right"
+            frameJson['hands'].append({'type':handtype, 'id':hand.id})
             print (handtype, hand.id, "{0:.2f}".format(hand.direction.x), "{0:.2f}".format(hand.direction.y), "{0:.2f}".format(hand.direction.z) )
+            
+        if(frame.hands):
+            self.client.publish("leapLesco", str(frameJson))
+
+
 
 def main():
     listener = LeapMotionListener()
